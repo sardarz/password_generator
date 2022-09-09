d174f0b98f7592ce8a95e5567ef763be0ca0db97
sardarz <muhkween@gmail.com>
2022-09-09 13:33:23 +0300

    before adding slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
-
+import {ReactComponent as Copy} from "./images/icon-copy.svg"
 function App() {
   return (
     <div className="App">
-      
+      <main className="main">
+        <div className="container">
+          <div className="main-content">
+            <h2 className="main-title">Password Generator</h2>
+            <div className="main-output">
+              <p className="main-password"></p>
+              <Copy />
+            </div>
+            <div className="generate">
+              <div className="generate-title-wrapper">
+                <p className="generate-title">Character Length</p>
+                <p className="generated-length">10</p>
+              </div>
+              <div className="slider">here goes the slider</div>
+              
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
